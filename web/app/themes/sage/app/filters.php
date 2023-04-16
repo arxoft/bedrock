4bd4791212d109ecedcb2c02de8a16689a5a83cc
Asif <asif@arxoft.com>
2023-04-16 21:51:49 +0500

    Basic starter Tailwind css theme on front page

--- web/app/themes/sage/app/filters.php
+++ web/app/themes/sage/app/filters.php
@@ -14,3 +14,8 @@ namespace App;
 add_filter('excerpt_more', function () {
     return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
 });
+
+
+add_filter('body_class', function($classes) {
+    return $classes[] = 'text-gray-800 antialiased';
+});
